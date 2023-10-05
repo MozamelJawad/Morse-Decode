1b15fe789ec4351552453d93c26822ca70347eb0
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-05 14:17:24 +0430

    add decode function for message

--- morse-decode.rb
+++ morse-decode.rb
@@ -50,3 +50,18 @@ end
 puts decode_word('-- -.--')
 
 # decode function
+def decode(morse_text)
+  words = morse_text.split('   ')
+  decoded_text = ''
+  words.each do |word|
+    decoded_text += "#{decode_word(word)} "
+  end
+  decoded_text.strip
+end
+
+# decoding messages
+my_name = '-- -.--   -. .- -- .'
+puts decode(my_name)
+
+morse = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode(morse)
